294b1d1dd3dc83c1d84214de440c4a216a39b366
jhoel <jhoel@jhoel>
2025-06-16 21:10:30 -0600

    semana 6

---
+++ src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -0,0 +1,19 @@
+package com.tienda.service;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-llc25.firebasestorage.app";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-llc25-firebase-adminsdk-fbsvc-a32326966c"+".json";
+}
--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Service
 public class CategoriaServiceImpl implements CategoriaService {
 
@@ -24,5 +23,22 @@ public class CategoriaServiceImpl implements CategoriaService {
         }
         return lista;
     }
-}
 
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
+    }
+}
